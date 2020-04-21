dc9a2fc24f8934ef62f414cbd0ad9248043f5128
Robert Metzger <rmetzger@apache.org>
2020-04-21 10:33:05 +0200

    improve tooling for azure

---
+++ tools/ci/install-snapshot-shaded.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# NOTE: Include this as a job in the job-templates.yml like this
+#
+#   - script: ./tools/ci/install-snapshot-shaded.sh https://github.com/rmetzger/flink-shaded.git FLINK-11086-hadoop3
+#    displayName: "Prepare flink-shaded"
+
+
+REPO=$1
+BRANCH=$2
+
+source ./tools/ci/maven_utils.sh
+
+git clone $REPO
+cd flink-shaded
+git checkout $BRANCH
+run_mvn install -Dhadoop.version=2.4.1
+run_mvn install -Dhadoop.version=2.6.5
+run_mvn install -Dhadoop.version=2.7.5
+run_mvn install -Dhadoop.version=2.8.3
+run_mvn install -Dhadoop.version=3.1.3
